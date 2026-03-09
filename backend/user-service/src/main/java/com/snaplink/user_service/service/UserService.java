fca087bd84545eafe57a3fff316e0d6c1339b8c8
akil-krishna <akileswarkrishnasamy@gmail.com>
2026-03-09 09:06:29 +0530

    Add controller, service and repository to user-service

---
+++ backend/user-service/src/main/java/com/snaplink/user_service/service/UserService.java
@@ -0,0 +1,13 @@
+package com.snaplink.user_service.service;
+
+import com.snaplink.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepo;
+
+}
