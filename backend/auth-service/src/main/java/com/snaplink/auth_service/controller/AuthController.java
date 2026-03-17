7c45c1ab805bd0a2b080e83878ac89cd8990e29a
akil-krishna <akileswarkrishnasamy@gmail.com>
2026-03-17 18:00:45 +0530

    Add methods to register user in AuthController and AuthService
    changes:
    	- Added /register endpoint in the AuthController and created registerUser method
    	- Added registerUser method in AuthService to registerUser

--- backend/auth-service/src/main/java/com/snaplink/auth_service/controller/AuthController.java
+++ backend/auth-service/src/main/java/com/snaplink/auth_service/controller/AuthController.java
@@ -1,9 +1,26 @@
 package com.snaplink.auth_service.controller;
 
+import com.snaplink.auth_service.dto.UserRegistrationRequestDTO;
+import com.snaplink.auth_service.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@Valid @RequestBody UserRegistrationRequestDTO userRegistrationRequestDTO){
+        authService.registerUser(userRegistrationRequestDTO);
+        return ResponseEntity.ok("User created successfully");
+    }
 }
--- backend/auth-service/src/main/java/com/snaplink/auth_service/service/AuthService.java
+++ backend/auth-service/src/main/java/com/snaplink/auth_service/service/AuthService.java
@@ -1,7 +1,23 @@
 package com.snaplink.auth_service.service;
 
+import com.snaplink.auth_service.dto.UserRegistrationRequestDTO;
+import com.snaplink.auth_service.mapper.AuthMapper;
+import com.snaplink.auth_service.model.Auth;
+import com.snaplink.auth_service.repository.AuthRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final AuthRepository authRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void registerUser(UserRegistrationRequestDTO userRegistrationRequestDTO){
+        Auth auth = AuthMapper.toEntity(userRegistrationRequestDTO);
+        auth.setPassword(passwordEncoder.encode(auth.getPassword()));
+        authRepository.save(auth);
+    }
 }
