215c6127491aa620e81abb3219e439c23d8bee0a
akil-krishna <akileswarkrishnasamy@gmail.com>
2026-03-26 19:26:02 +0530

    Add JwtAuthenticationFilter class
    changes:
    	- Added path based token extraction
    	- validate the token with JwtService
    	- Add user id to the request header
    	- return status as unauthorized if token is invalid or expired or missing

---
+++ backend/api-gateway/src/main/java/com/snaplink/api_gateway/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.snaplink.api_gateway.filters;
+
+import com.snaplink.api_gateway.service.JwtService;
+import io.jsonwebtoken.JwtException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter implements GlobalFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getPath().toString();
+        String token = null;
+
+        if (path.equals("/auth/login")) {
+            return chain.filter(exchange);
+        } else if (path.equals("/auth/refresh")) {
+            HttpCookie cookie = exchange.getRequest().getCookies().getFirst("refresh-token");
+            if (cookie == null) return exchange.getResponse().setComplete();
+            token = cookie.getValue();
+        } else {
+            String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+            if(authHeader == null || !authHeader.startsWith("Bearer ")){
+                return returnUnauthorized(exchange);
+            }
+            token = authHeader.substring(7);
+        }
+
+        try {
+            long userId = jwtService.getUserId(token);
+            ServerHttpRequest mutatedRequest = exchange.getRequest()
+                    .mutate()
+                    .header("x-User-Id", String.valueOf(userId))
+                    .build();
+            return chain.filter(exchange.mutate().request(mutatedRequest).build());
+
+        } catch (Exception ex){
+            return returnUnauthorized(exchange);
+        }
+
+    }
+
+    private Mono<Void> returnUnauthorized(ServerWebExchange exchange){
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+}
